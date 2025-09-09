26013f0db4b80d871b4390a099e38816d340d66d
Denc-revolutions <antonshershin@gmail.com>
2025-09-09 13:25:14 +0300

    lesson02-01

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,6 +13,14 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+let isAccess = false
 
 // your code
+
+while(!isAccess) {
+    if(isAdmin === true || isVerifiedUser === true && hasSpecialPermission === true || hasTemporaryPass === true ) {
+        isAccess = true
+    } else {
+        isAccess = false
+    }
+}
