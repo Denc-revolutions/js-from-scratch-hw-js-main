e204838718874c6922bd16a37dbcde0530f81e9a
Denc-revolutions <antonshershin@gmail.com>
2025-09-23 16:53:29 +0300

    fix task 03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -3,7 +3,7 @@
 function findLargest(a, b, c) {
     if(a >= b && a >= c) {
         return a
-    } else if(b <= a && b <= c) {
+    } else if(b >= a && b >= c) {
         return b
     } else {
         return c
